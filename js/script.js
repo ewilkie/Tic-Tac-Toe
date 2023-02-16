6798fbc6b49c7bf98dc8fde4b83f6783e35fc4b1
ewilkie <emilie.wilkie@gmail.com>
2023-02-16 14:24:43 +1100

    end game and rest button

--- js/script.js
+++ js/script.js
@@ -36,8 +36,19 @@ const WINNING_COMBINATIONS = [
 
 
 /* Functionality for when specific square is clicked */
-const cells = document.querySelectorAll('.base-child');
-const cellElements = Array.from(cells)
+let cells = document.querySelectorAll('.base-child');
+
+// need array for winning combinations check based on index
+let cellElements = Array.from(cells)
+
+// this needs to be interactively changed based on either user input or who goes first. 
+// doesn't really matter, leave this for now will be defined elsewhere
+
+let isPlayer_O_Turn = false
+
+// need to set this up during start game
+let PLAYER_O_CLASS = "O"
+let PLAYER_X_CLASS = "X"
 
 
 // checks whether any of the winning combinations through 'some' contain all current class list in cellElements. 
@@ -52,20 +63,6 @@ function checkWin(currentClass) {
 }
 
 
-cells.forEach(cell => {
-    cell.addEventListener('click', handleClick);
-});
-  
-// this needs to be interactively changed based on either user input or who goes first. 
-// doesn't really matter, leave this for now will be defined elsewhere
-let fig = "cross"
-let isPlayer_O_Turn = false
-
-// need to set this up during start game
-let PLAYER_O_CLASS = "O"
-let PLAYER_X_CLASS = "X"
-
-
 function handleClick() {
 
   // check who's turn it is
@@ -78,24 +75,19 @@ function handleClick() {
         c.classList.toggle('hidden')
 
         this.classList.add(PLAYER_X_CLASS)
-        console.log(checkWin(currentClass))
 
         // of returns true, end game 
         if (checkWin(currentClass)){
           endGame();
         }
-
         // swap classes
         isPlayer_O_Turn = true
     } else {
         let c = this.querySelector(".circle");
         c.classList.toggle('hidden')
-        fig = "cross";
-        this.classList.add("circle")
         
         // add class list to determine winning
         this.classList.add(PLAYER_O_CLASS)
-        console.log(checkWin(currentClass))
 
         // of returns true, end game 
         if (checkWin(currentClass)){
@@ -111,6 +103,10 @@ function handleClick() {
 
 }
 
+cells.forEach(cell => {
+  cell.addEventListener('click', handleClick);
+});
+
 const winningMessageElement = document.querySelector('.results')
 const winningMessageText = document.querySelector('#winningMessageText')
 
@@ -119,23 +115,44 @@ function endGame() {
   winningMessageElement.classList.add('show');
 }
 
-
-
+/* ================================ Reset Game ============================= */
 
 function resetGrid() {
-    // Remove the highlight class from all cells
-    cells.forEach(cell => {
-        cell.classList.add('hidden');
-    });
+  // Remove classes
+  cells.forEach(cell => {
+      // make cross hidden
+      let cross = cell.querySelector(".cross");
+      cross.classList.add('hidden');
 
-    // Add the click event listener back to all cells
-    cells.forEach(cell => {
-        cell.addEventListener('click', handleClick);
-    });
+      // make circle hidden 
+      let circle = cell.querySelector(".circle");
+      circle.classList.add('hidden');
+
+      // remove classess added to determine winning combination
+      cell.classList.remove(PLAYER_O_CLASS);
+      cell.classList.remove(PLAYER_X_CLASS);
+  });
+
+  // Add the click event listener back to all cells
+  cells.forEach(cell => {
+      cell.addEventListener('click', handleClick);
+  });
 }
 
 
-/* this adds event listeners to each square 
+let buttonReset = document.querySelector('#resetbutton');
+
+function resetGame() {
+  winningMessageElement.classList.remove('show');
+  resetGrid()
+}
+
+buttonReset.addEventListener('click', resetGame);
+
+
+
+/*
+ this adds event listeners to each square 
 const cells = document.querySelectorAll('.base-child');
 
 cells.forEach(cell => {
